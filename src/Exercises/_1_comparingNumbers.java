7f76d1b7837ee10fcf8a16635c8d802e324e8f56
daben <dabenegu@gmail.ciom>
2024-08-31 13:12:54 +0200

    conditionalsAndExercises

---
+++ src/Exercises/_1_comparingNumbers.java
@@ -0,0 +1,25 @@
+package Exercises;
+/*
+Write a program that compares two numbers. Use any comparison operator
+Create 3 variables.
+2 variables to store both numbers and 1 variable store the result
+Print the result
+*/
+public class _1_comparingNumbers {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 9;
+        String result = "";
+
+        if(x > y){
+            result = "x is bigger than y";
+        }else if(x == y){
+            result = "x is equal to y";
+        }else{
+            result = "y is bigger than x";
+        }
+
+        System.out.println(result);
+
+    }
+}
---
+++ src/Exercises/_2_comparingStrings.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class _2_comparingStrings {
+    public static void main(String[] args) {
+        String name1 = "David";
+        String name2 = "David";
+        
+
+        //if(name1.equals(name2)){
+            //result
+        //}
+    }
+}
