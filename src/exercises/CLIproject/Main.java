5aa64e5afeff9dd0842bd7b109a49157c7707b3f
daben <dabenegu@gmail.ciom>
2024-09-26 16:39:06 +0200

    Planing

--- src/exercises/CLIproject/Main.java
+++ src/exercises/CLIproject/Main.java
@@ -2,6 +2,8 @@ package exercises.CLIproject;
 
 public class Main {
     public static void main(String[] args) {
+        // Here the menu will be displayed (using a switch statement)
+
 
     }
 }
